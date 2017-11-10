e56e9f03221cf00d115c9f7e68186badfa97d072
ETY0004 <ETY0004@DESKTOP-A5USBB3>
2017-11-10 16:28:39 +0100

    Exos 4 et 5

--- src/Menu.java
+++ src/Menu.java
@@ -1,8 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu {
+	
+	private static ArrayList<Integer> nombres;
 
 	public static void main(String[] args) {
+		nombres = new ArrayList<Integer>();
+		System.out.println("===EXERCICE 4===");
+		while (true) {
+			callMenu();			
+		}
+	}
+	
+	public static void callMenu() {
+		System.out.println("\nEntrez une option :\n"
+				+ "1) Ajouter un nombre\n"
+				+ "2) Voir les nombres entrés");
 		
+		Scanner sc = new Scanner(System.in);
+		int choix = sc.nextInt();
+		
+		switch (choix) {
+		case 1:
+			System.out.println("Entrez un nombre :");
+			int nombre = sc.nextInt();
+			nombres.add(nombre);
+			break;
+		case 2:
+			System.out.println("Voici les nombres entrés :");
+			for (int i : nombres) {
+				System.out.print(i + " ");
+			}
+			break;
+		default:
+			System.out.println("Entrez une option valide.");
+		}
 	}
 
 }
---
+++ src/TestFibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TestFibonacci {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("===EXERCICE 5===\n"
+				+ "Choisissez un rang de la suite de Fibonacci :");
+		int rang = sc.nextInt();
+		System.out.println("Rang "+ rang +" de la suite de Fibonacci = " + fibonacci(rang) );
+		
+	}
+
+	private static int fibonacci(int rang) {
+		if (rang == 0 || rang == 1) {
+			return rang;
+		} else if (rang > 1) {
+			return fibonacci(rang-1) + fibonacci(rang-2);
+		} else {
+			return (int)Math.pow(-1, rang-1)*fibonacci(-rang);
+		}
+	}
+
+}
